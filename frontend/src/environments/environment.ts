2f2d4288a76b783129d8f7eac5eb851e0daabd6b
Albertino <albertino.haingonirina@gmail.com>
2026-03-20 14:21:10 +0300

    last commit test-node

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Correction ici : vérifie bien s'il manque le "end" ou s'il y a un tiret
-  apiUrl: "https://test-nodeback.vercel.app/api",
+  apiUrl: "https://test-nodeback-tinohzens-projects.vercel.app/api", // L'URL qui a renvoyé []
 };
