dc9d4bc624db72951b174f37f94f3f9670d84ca8
Albertino <albertino.haingonirina@gmail.com>
2026-03-20 11:45:38 +0300

    last commit test-node

--- backend/app.ts
+++ backend/app.ts
@@ -5,7 +5,13 @@ import apiRoutes from "./routes/api.routes.js";
 const app = express();
 
 // Configuration CORS : Autorise les requêtes provenant de ton futur frontend Vercel
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://test-nodefrontend.vercel.app", //  URL frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Middleware pour lire le JSON dans les requêtes POST/PUT
 app.use(express.json());
